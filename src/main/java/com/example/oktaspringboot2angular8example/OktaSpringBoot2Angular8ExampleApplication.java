4de8ac073297cab9665b0481580534c00a7db4c7
Eduardo Assis da Silva <eduardouflassis@gmail.com>
2020-09-23 17:41:28 +0100

    Creating project

---
+++ src/main/java/com/example/oktaspringboot2angular8example/OktaSpringBoot2Angular8ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.oktaspringboot2angular8example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktaSpringBoot2Angular8ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OktaSpringBoot2Angular8ExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oktaspringboot2angular8example/OktaSpringBoot2Angular8ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oktaspringboot2angular8example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktaSpringBoot2Angular8ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
